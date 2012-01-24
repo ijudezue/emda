9a11029adb61f5154d8034e9d80027e36f9a44da
heiseuj@gmail.com <heiseuj@gmail.com@246d29fb-aed4-215a-132e-b7b9ee0c06e5>
2012-01-24 19:16:59 +0000

    added first boot of emda -setup, and remove after a successful setup. Fixes issue 32

--- branches/heiseuj/makeISO.sh
+++ branches/heiseuj/makeISO.sh
@@ -7,14 +7,14 @@ ls -sf /home/emda/workspace/src/trunk/emda_install.img/emda/var/www/html/fpdf152
 ls -sf /home/emda/workspace/src/trunk/emda_install.img/emda/var/www/html/jpgraph-1.12.1 /home/emda/workspace/src/trunk/emda_install.img/emda/var/www/html/jpgraph 
 
 #make i386 EMDA $emdaVer
-rsync -ar /home/emda/workspace/src/trunk/emda_install.img/emda /home/emda/workspace/src/trunk/emda_install.img/i386/
-rsync --archive /home/emda/workspace/src/trunk/repodata/comps.xml /home/emda/workspace/builds/EMDA-i386/repodata/
+rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/emda_install.img/emda /home/emda/workspace/src/trunk/emda_install.img/i386/
+rsync --archive --exclude '.svn' /home/emda/workspace/src/trunk/repodata/comps.xml /home/emda/workspace/builds/EMDA-i386/repodata/
 mksquashfs /home/emda/workspace/src/trunk/emda_install.img/i386 /home/emda/workspace/builds/EMDA-i386/images/install.img -noappend -no-sparse -no-recovery -info -no-fragments -no-duplicates
-rsync -ar /home/emda/workspace/src/trunk/isolinux/* /home/emda/workspace/builds/EMDA-i386/isolinux/;
+rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/isolinux/* /home/emda/workspace/builds/EMDA-i386/isolinux/;
 sed -i "/menu title Welcome to EMDA ver arch Bit!/ c\menu title Welcome to EMDA $emdaVer 32 Bit!" /home/emda/workspace/builds/EMDA-i386/isolinux/isolinux.cfg
 sed -i "/  menu label ^Install EMDA ver arch/ c\  menu label ^Install EMDA $emdaVer i386" /home/emda/workspace/builds/EMDA-i386/isolinux/isolinux.cfg
-rsync -ar /home/emda/workspace/src/trunk/repodata/* /home/emda/workspace/builds/EMDA-i386/repodata/;
-rsync -ar /home/emda/workspace/src/trunk/Packages/* /home/emda/workspace/builds/EMDA-i386/Packages/;
+rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/repodata/* /home/emda/workspace/builds/EMDA-i386/repodata/;
+rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/Packages/* /home/emda/workspace/builds/EMDA-i386/Packages/;
 #create the repo tree for i386
 cd /home/emda/workspace/builds/EMDA-i386
 createrepo -g repodata/comps.xml .
@@ -26,14 +26,14 @@ rm -fr /home/emda/workspace/builds/EMDA-i386/install.img
 rm -fr /home/emda/workspace/builds/EMDA-i386/repodata/*
 
 #make x86_64 EMDA $emdaVer
-rsync -ar /home/emda/workspace/src/trunk/emda_install.img/emda /home/emda/workspace/src/trunk/emda_install.img/x86_64/
-rsync --archive /home/emda/workspace/src/trunk/repodata/comps.xml /home/emda/workspace/builds/EMDA-x86_64/repodata/
+rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/emda_install.img/emda /home/emda/workspace/src/trunk/emda_install.img/x86_64/
+rsync --archive --exclude '.svn' /home/emda/workspace/src/trunk/repodata/comps.xml /home/emda/workspace/builds/EMDA-x86_64/repodata/
 mksquashfs /home/emda/workspace/src/trunk/emda_install.img/x86_64 /home/emda/workspace/builds/EMDA-x86_64/images/install.img -noappend -no-sparse -no-recovery -info -no-fragments -no-duplicates
-rsync -ar /home/emda/workspace/src/trunk/isolinux/* /home/emda/workspace/builds/EMDA-x86_64/isolinux/;
+rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/isolinux/* /home/emda/workspace/builds/EMDA-x86_64/isolinux/;
 sed -i "/menu title Welcome to EMDA ver arch Bit!/ c\menu title Welcome to EMDA $emdaVer 64 Bit!" /home/emda/workspace/builds/EMDA-x86_64/isolinux/isolinux.cfg
 sed -i "/  menu label ^Install EMDA ver arch/ c\  menu label ^Install EMDA $emdaVer x86_64" /home/emda/workspace/builds/EMDA-x86_64/isolinux/isolinux.cfg
-rsync -ar /home/emda/workspace/src/trunk/repodata/* /home/emda/workspace/builds/EMDA-x86_64/repodata/;
-rsync -ar /home/emda/workspace/src/trunk/Packages/* /home/emda/workspace/builds/EMDA-x86_64/Packages/;
+rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/repodata/* /home/emda/workspace/builds/EMDA-x86_64/repodata/;
+rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/Packages/* /home/emda/workspace/builds/EMDA-x86_64/Packages/;
 #create the repo tree for x86_64
 cd /home/emda/workspace/builds/EMDA-x86_64
 createrepo -g repodata/comps.xml .
