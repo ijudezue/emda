db1cec8f8c3014531ebd414473b9cc45a470e67b
antzero8000 <antzero8000@gmail.com>
2012-02-15 16:04:25 +0000

    postfix install with mysql ... not for production

--- emda_install.img/emda/root/mailwatch_user.sql
+++
@@ -1,3 +0,0 @@
-GRANT ALL ON emdadb.* TO emdadb@localhost IDENTIFIED BY '<password>'; 
-FLUSH PRIVILEGES;
-INSERT INTO users SET username = 'emdadmin', password = md5('<password>'), fullname = 'EMDAdmin', type ='A';
