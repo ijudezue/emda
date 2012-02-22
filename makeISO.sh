13c4489f9bb61e6097e7d16065bf12c2863f0fb2
heiseuj <heiseuj@gmail.com>
2012-02-22 19:31:12 +0000

    checking in changes so far, along with a mostly working installer for a developer system.

--- makeISO.sh
+++ makeISO.sh
@@ -17,147 +17,102 @@
 # see http://www.gnu.org/licenses/.
 
 
-emdaVer="`cat /home/emda/workspace/src/trunk/version`"
-echo $emdaVer > /home/emda/workspace/src/trunk/emda_install.img/emda/etc/emdaVersion
+homePath="`whoami`"
+ver="1.0"
+
+if [ "$homePath" == "root" ]
+then
+	homePath="/root"
+else
+	homePath="/home/$homePath"
+fi
+
+#workspacePath="$homePath/EMDAworkspace-$ver"
+workspacePath="$homePath/EMDAworkspace"
+buildPath="$workspacePath/builds"
+isoPath="$workspacePath/iso"
+isoMountPath="$workspacePath/tmp"
+
+#change "trunk" to whatever you named your branch from google code svn
+#$srcPath is the actual location (root path) of the checked out files from subversion.
+# basically the root path to makeISO.sh
+#srcPath="$workspacePath/src/trunk"
+srcPath="`pwd`"
+
+emdaImagePath="$srcPath/emda_install.img"
+
+emdaVer="`cat $srcPath/version`"
+echo $emdaVer > $emdaImagePath/emda/etc/emdaVersion
+
 #create these symlinks since eclipse/svn doesn't like keeping symlinks 
-ln -sf /var/www/html/fpdf152 /home/emda/workspace/src/trunk/emda_install.img/emda/var/www/html/fpdf
-ln -sf /var/www/html/jpgraph-1.12.1 /home/emda/workspace/src/trunk/emda_install.img/emda/var/www/html/jpgraph 
+ln -sf /var/www/html/fpdf152 $emdaImagePath/emda/var/www/html/fpdf
+ln -sf /var/www/html/jpgraph-1.12.1 $emdaImagePath/emda/var/www/html/jpgraph
 
-#make i386 EMDA $emdaVer
-arch="i386"
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/emda_install.img/emda /home/emda/workspace/src/trunk/emda_install.img/$arch/
-#fixing permissions
-	#User ID: apache=48, postfix=89
-	#Group ID: apache=48, postfix=89, postdrop=90, clam=498
-	sudo chown 0:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda
+func_makeIso(){
+	sudo rsync -ar --exclude '.svn' $emdaImagePath/emda $buildPath/installImg-$1/
+	#fixing permissions
+		#User ID: apache=48, postfix=89
+		#Group ID: apache=48, postfix=89, postdrop=90, clam=498
+		sudo chown 0:0 -R $buildPath/installImg-$1/emda
+		
+		sudo chown 0:48 -R $buildPath/installImg-$1/emda/etc/mail/spamassassin/razor
+			sudo chmod 770 -R $buildPath/installImg-$1/emda/etc/mail/spamassassin/razor
+		sudo chown 0:48 -R $buildPath/installImg-$1/emda/etc/MailScanner/bayes
+			sudo chmod 774 -R $buildPath/installImg-$1/emda/etc/MailScanner/bayes
 	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/root
+			sudo chmod 755 -R $buildPath/installImg-$1/emda/usr/share
+			sudo chmod 744 -R $buildPath/installImg-$1/emda/usr/share/mailgraph/mailgraph.css
 	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc
-	sudo chown 0:48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc/mail/spamassassin/razor
-		sudo chmod 770 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc/mail/spamassassin/razor
-	sudo chown 0:48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc/MailScanner/bayes
-		sudo chmod 774 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc/MailScanner/bayes
+		sudo chown 89:0 $buildPath/installImg-$1/emda/var/spool/postfix/*
+		sudo chown :90 -R $buildPath/installImg-$1/emda/var/spool/postfix/public
+		sudo chown :90 -R $buildPath/installImg-$1/emda/var/spool/postfix/maildrop
+		sudo chown :48 -R $buildPath/installImg-$1/emda/var/spool/postfix/incoming
+		sudo chown :48 -R $buildPath/installImg-$1/emda/var/spool/postfix/hold
+		sudo chown :48 -R $buildPath/installImg-$1/emda/var/spool/postfix/outgoing
+			sudo chmod 740 $buildPath/installImg-$1/emda/var/spool/postfix/incoming
+			sudo chmod 740 $buildPath/installImg-$1/emda/var/spool/postfix/hold
+			sudo chmod 740 $buildPath/installImg-$1/emda/var/spool/postfix/outgoing
 	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr
+		sudo chown 89:498 $buildPath/installImg-$1/emda/var/spool/MailScanner/*
+		sudo chown 89:48 $buildPath/installImg-$1/emda/var/spool/MailScanner/quarantine
 	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr/share
-		sudo chmod 755 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr/share
-		sudo chmod 744 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr/share/mailgraph/mailgraph.css		
+		sudo chown 48:48 -R $buildPath/installImg-$1/emda/var/www/.spamassassin
+		sudo chown 48:48 -R $buildPath/installImg-$1/emda/var/www
+		sudo chmod 755 -R $buildPath/installImg-$1/emda/var/www/cgi-bin
+		sudo chmod ug+rwx $buildPath/installImg-$1/emda/var/www/html/images
+		sudo chmod ug+rwx $buildPath/installImg-$1/emda/var/www/html/images/cache
+		 
 	
-	sudo chown 0:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr/sbin/MailScanner
-	
-	sudo chown 0:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix
-	sudo chown 89:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/*
-	sudo chown :90 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/public
-	sudo chown :90 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/maildrop
-	sudo chown :48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/incoming
-	sudo chown :48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/hold
-	sudo chown :48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/outgoing
-		sudo chmod 740 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/incoming
-		sudo chmod 740 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/hold
-		sudo chmod 740 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/outgoing
+	sudo rsync -ar --exclude '.svn' $srcPath/repodata/comps.xml $buildPath/EMDA-$1/repodata/
+	sudo mksquashfs $buildPath/installImg-$1 $buildPath/EMDA-$1/images/install.img -noappend -no-sparse -no-recovery -info -no-fragments -no-duplicates
+	sudo rsync -ar --exclude '.svn' $srcPath/isolinux/* $buildPath/EMDA-$1/isolinux/;
+	sudo sed -i "/menu title Welcome to EMDA ver arch Bit!/ c\menu title Welcome to EMDA $emdaVer $1" $buildPath/EMDA-$1/isolinux/isolinux.cfg
+	sudo sed -i "/  menu label ^Install EMDA ver arch/ c\  menu label ^Install EMDA $emdaVer $1" $buildPath/EMDA-$1/isolinux/isolinux.cfg
+	sudo rsync -ar --exclude '.svn' $srcPath/repodata/* $buildPath/EMDA-$1/repodata/;
+	sudo rsync -ar --exclude '.svn' $srcPath/Packages/* $buildPath/EMDA-$1/Packages/;
+	#create the repo tree
+	cd $buildPath/EMDA-$1
+	sudo createrepo -g repodata/comps.xml .
+	sudo mkisofs -r -R -J -T -v -no-emul-boot -boot-load-size 4 -boot-info-table -V "EMDA $emdaVer " -p "Team EMDA" -A "EMDA $emdaVer " -b isolinux/isolinux.bin -c isolinux/boot.cat -x "lost+found" -x "makeISO.*" -x "~*" -x "*~" -x "*.sh" -o $isoPath/EMDA-$emdaVer-$1.iso .;
+	#cleanup
+	sudo rm -fr $buildPath/installImg-$1/emda
+	sudo rm -fr $buildPath/EMDA-$1/isolinux/isolinux.cfg
+	sudo rm -fr $buildPath/EMDA-$1/install.img
+	sudo rm -fr $buildPath/EMDA-$1/repodata/*
+}
 
-	sudo chown 0:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/MailScanner
-	sudo chown 89:498 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/MailScanner/*
-	sudo chown 89:48 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/MailScanner/quarantine	
-	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/lib/spamassassin	
-	
-	sudo chown 48:48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www/.spamassassin
-	sudo chown 48:48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www
-	sudo chmod 755 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www/cgi-bin
-	sudo chmod ug+rwx /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www/html/images
-	sudo chmod ug+rwx /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www/html/images/cache
-	 
+main(){
+	func_makeIso i386
+	func_makeIso x86_64
 
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/repodata/comps.xml /home/emda/workspace/builds/EMDA-$arch/repodata/
-mksquashfs /home/emda/workspace/src/trunk/emda_install.img/$arch /home/emda/workspace/builds/EMDA-$arch/images/install.img -noappend -no-sparse -no-recovery -info -no-fragments -no-duplicates
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/isolinux/* /home/emda/workspace/builds/EMDA-$arch/isolinux/;
-sed -i "/menu title Welcome to EMDA ver arch Bit!/ c\menu title Welcome to EMDA $emdaVer 32 Bit!" /home/emda/workspace/builds/EMDA-$arch/isolinux/isolinux.cfg
-sed -i "/  menu label ^Install EMDA ver arch/ c\  menu label ^Install EMDA $emdaVer $arch" /home/emda/workspace/builds/EMDA-$arch/isolinux/isolinux.cfg
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/repodata/* /home/emda/workspace/builds/EMDA-$arch/repodata/;
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/Packages/* /home/emda/workspace/builds/EMDA-$arch/Packages/;
-#create the repo tree for i386
-cd /home/emda/workspace/builds/EMDA-$arch
-createrepo -g repodata/comps.xml .
-mkisofs -r -R -J -T -v -no-emul-boot -boot-load-size 4 -boot-info-table -V "EMDA $emdaVer " -p "Team EMDA" -A "EMDA $emdaVer " -b isolinux/isolinux.bin -c isolinux/boot.cat -x "lost+found" -x "makeISO.*" -x "~*" -x "*~" -x "*.sh" -o /home/emda/workspace/iso/EMDA-$emdaVer-$arch.iso .;
-#cleanup
-sudo rm -fr /home/emda/workspace/src/trunk/emda_install.img/$arch/emda
-rm -fr /home/emda/workspace/builds/EMDA-$arch/isolinux/isolinux.cfg
-rm -fr /home/emda/workspace/builds/EMDA-$arch/install.img
-rm -fr /home/emda/workspace/builds/EMDA-$arch/repodata/*
+	#clean up local links
+	sudo rm $emdaImagePath/emda/var/www/html/fpdf
+	sudo rm $emdaImagePath/emda/var/www/html/jpgraph
+	#clean out version info
+	sudo rm $emdaImagePath/emda/etc/emdaVersion
+}
 
-#make x86_64 EMDA $emdaVer
-arch="x86_64"
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/emda_install.img/emda /home/emda/workspace/src/trunk/emda_install.img/$arch/
-#fixing permissions
-	#User ID: apache=48, postfix=89
-	#Group ID: apache=48, postfix=89, postdrop=90, clam=498
-	sudo chown 0:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda
-	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/root
-	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc
-	sudo chown 0:48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc/mail/spamassassin/razor
-		sudo chmod 770 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc/mail/spamassassin/razor
-	sudo chown 0:48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/etc/MailScanner/bayes
-		sudo chmod 774 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/etc/MailScanner/bayes
-	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr
-	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr/share
-		sudo chmod 755 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr/share
-		sudo chmod 744 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr/share/mailgraph/mailgraph.css		
-	
-	sudo chown 0:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/usr/sbin/MailScanner
-	
-	sudo chown 0:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix
-	sudo chown 89:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/*
-	sudo chown :90 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/public
-	sudo chown :90 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/maildrop
-	sudo chown :48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/incoming
-	sudo chown :48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/hold
-	sudo chown :48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/outgoing
-		sudo chmod 740 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/incoming
-		sudo chmod 740 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/hold
-		sudo chmod 740 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/postfix/outgoing
+main
 
-	sudo chown 0:0 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/MailScanner
-	sudo chown 89:498 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/MailScanner/*
-	sudo chown 89:48 /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/spool/MailScanner/quarantine
-	
-	sudo chown 0:0 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/lib/spamassassin	
-	
-	sudo chown 48:48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www/.spamassassin
-	sudo chown 48:48 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www
-	sudo chmod 755 -R /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www/cgi-bin
-	sudo chmod ug+rwx /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www/html/images
-	sudo chmod ug+rwx /home/emda/workspace/src/trunk/emda_install.img/$arch/emda/var/www/html/images/cache
-
-
-rsync --archive --exclude '.svn' /home/emda/workspace/src/trunk/repodata/comps.xml /home/emda/workspace/builds/EMDA-$arch/repodata/
-mksquashfs /home/emda/workspace/src/trunk/emda_install.img/$arch /home/emda/workspace/builds/EMDA-$arch/images/install.img -noappend -no-sparse -no-recovery -info -no-fragments -no-duplicates
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/isolinux/* /home/emda/workspace/builds/EMDA-$arch/isolinux/;
-sed -i "/menu title Welcome to EMDA ver arch Bit!/ c\menu title Welcome to EMDA $emdaVer 64 Bit!" /home/emda/workspace/builds/EMDA-$arch/isolinux/isolinux.cfg
-sed -i "/  menu label ^Install EMDA ver arch/ c\  menu label ^Install EMDA $emdaVer $arch" /home/emda/workspace/builds/EMDA-$arch/isolinux/isolinux.cfg
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/repodata/* /home/emda/workspace/builds/EMDA-$arch/repodata/;
-rsync -ar --exclude '.svn' /home/emda/workspace/src/trunk/Packages/* /home/emda/workspace/builds/EMDA-$arch/Packages/;
-#create the repo tree for x86_64
-cd /home/emda/workspace/builds/EMDA-$arch
-createrepo -g repodata/comps.xml .
-mkisofs -r -R -J -T -v -no-emul-boot -boot-load-size 4 -boot-info-table -V "EMDA $emdaVer $arch" -p "Team EMDA" -A "EMDA $emdaVer $arch" -b isolinux/isolinux.bin -c isolinux/boot.cat -x "lost+found" -x "makeISO.*" -x "~*" -x "*~" -x "*.sh" -o /home/emda/workspace/iso/EMDA-$emdaVer-$arch.iso .;
-#cleanup
-sudo rm -fr /home/emda/workspace/src/trunk/emda_install.img/$arch/emda
-rm -fr /home/emda/workspace/builds/EMDA-$arch/isolinux/isolinux.cfg
-rm -fr /home/emda/workspace/builds/EMDA-$arch/install.img
-rm -fr /home/emda/workspace/builds/EMDA-$arch/repodata/*
-
-
-#clean up local links
-rm /home/emda/workspace/src/trunk/emda_install.img/emda/var/www/html/fpdf
-rm /home/emda/workspace/src/trunk/emda_install.img/emda/var/www/html/jpgraph
-#clean out version info
-rm /home/emda/workspace/src/trunk/emda_install.img/emda/etc/emdaVersion
-
+sudo -K
 exit 0;
